01bf59b02b4cd8ee865a5f61657c9c8f96128714
Quyền Lê Văn <quyenlevan97@gmail.com>
2021-06-24 03:29:39 +0000

    Update 1624503806537-PersonMigration.ts

--- src/migration/1624503806537-PersonMigration.ts
+++ src/migration/1624503806537-PersonMigration.ts
@@ -3,11 +3,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class PersonMigration1624503806537 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Family" RENAME COLUMN "desc" TO "name"`);
+        await queryRunner.query(`ALTER TABLE "Person" RENAME COLUMN "firstName" TO "name"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Family" RENAME COLUMN "name" TO "desc"`)
+        await queryRunner.query(`ALTER TABLE "Person" RENAME COLUMN "name" TO "firstName"`)
     }
 
 }
